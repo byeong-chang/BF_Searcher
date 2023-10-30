0381c62c03c1686b27b209b45631612898826e55
wooshanghyeon <woosh9912@naver.com>
2023-10-30 20:26:37 +0900

    앱 프로젝트 생성

---
+++ App_BFSearcher/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>소프트웨어 공학 10조</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
