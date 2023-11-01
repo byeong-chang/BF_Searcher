a74c247cb7aa6350e94add45d28d0b5075e22a58
ByeongChang <als6068@naver.com>
2023-11-01 14:44:46 +0900

    Spring 개발 설정
    기본 개발 설정을 완료했습니다.

---
+++ Backend/bfSearcher/src/main/java/softwareEngineering/bfSearcher/BfSearcherApplication.java
@@ -0,0 +1,13 @@
+package softwareEngineering.bfSearcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BfSearcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BfSearcherApplication.class, args);
+	}
+
+}
---
+++ Backend/bfSearcher/src/test/java/softwareEngineering/bfSearcher/BfSearcherApplicationTests.java
@@ -0,0 +1,13 @@
+package softwareEngineering.bfSearcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BfSearcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
