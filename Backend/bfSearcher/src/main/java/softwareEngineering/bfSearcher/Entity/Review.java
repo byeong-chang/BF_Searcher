427a1fb87e824d1f88afcea48107536fce002a06
ByeongChang <als6068@naver.com>
2023-11-07 15:42:16 +0900

    추가 테이블 연동용 Entity 작성
    추가 테이블 연동용 Entity 작성

---
+++ Backend/bfSearcher/src/main/java/softwareEngineering/bfSearcher/Entity/Review.java
@@ -0,0 +1,35 @@
+package softwareEngineering.bfSearcher.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "review")
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "location_id")
+    private Location location;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "star_rating")
+    private Double starRating;
+
+    @Column(name = "image")
+    private String image;
+
+    @Column(name = "content")
+    private String content;
+}
