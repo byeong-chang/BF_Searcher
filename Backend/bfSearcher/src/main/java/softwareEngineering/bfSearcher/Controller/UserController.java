5c3713caff67cef858bc07c3e48ad4a0e2c66b01
ByeongChang <als6068@naver.com>
2023-11-08 17:34:09 +0900

    회원가입, 로그인, 자동 로그인(접속) 구현
    UserRepository
    User 데이터를 DB에 저장하는 Save 구현
    userId,Passwd 기준으로 User 데이터를 찾아와서 반환하는 findbyUserIdAndPasswd 구현
    Token 기준으로 user 데이터 찾는 findByToken 구현
    UserService
    Entity To Dto 코드 작성
    회원가입, 로그인 , 자동 로그인(Access) 구현

--- Backend/bfSearcher/src/main/java/softwareEngineering/bfSearcher/Controller/UserController.java
+++ Backend/bfSearcher/src/main/java/softwareEngineering/bfSearcher/Controller/UserController.java
@@ -1,8 +1,9 @@
 package softwareEngineering.bfSearcher.Controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import softwareEngineering.bfSearcher.DTO.UserDto;
+import softwareEngineering.bfSearcher.Entity.User;
 import softwareEngineering.bfSearcher.Service.UserService;
 
 @RestController
@@ -11,4 +12,17 @@ import softwareEngineering.bfSearcher.Service.UserService;
 public class UserController {
     private final UserService userService;
 
+    @PostMapping("sign_up")
+    public String SignUp(@RequestBody UserDto userDto){
+        return userService.SignUp(userDto);
+    }
+
+    @PostMapping("Login")
+    public User Login(@RequestBody UserDto userDto){
+        return userService.Login(userDto);
+    }
+    @GetMapping("Acess/{token}")
+    public User Acecss(@PathVariable String token){
+        return userService.Access(token);
+    }
 }
--- Backend/bfSearcher/src/main/java/softwareEngineering/bfSearcher/Repository/UserRepository.java
+++ Backend/bfSearcher/src/main/java/softwareEngineering/bfSearcher/Repository/UserRepository.java
@@ -3,6 +3,12 @@ package softwareEngineering.bfSearcher.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import softwareEngineering.bfSearcher.Entity.User;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    User save(User user);
+    Optional<User> findByUserIdAndPasswd(String userId, String passwd);
+    Optional<User> findByToken(String token);
 }
