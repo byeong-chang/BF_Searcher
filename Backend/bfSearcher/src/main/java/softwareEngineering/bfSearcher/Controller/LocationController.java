8172f7b501801b58d39e3273451ee8b098ec8051
johyoji <hyoji4992@naver.com>
2023-11-11 16:49:25 +0900

    장소 추천 GET API 추가

---
+++ Backend/bfSearcher/src/main/java/softwareEngineering/bfSearcher/Controller/LocationController.java
@@ -0,0 +1,23 @@
+package softwareEngineering.bfSearcher.Controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import softwareEngineering.bfSearcher.DTO.LocationDto;
+import softwareEngineering.bfSearcher.Service.LocationService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("")
+@RequiredArgsConstructor
+public class LocationController {
+    private final LocationService locationService;
+
+    @GetMapping("Location/{token}")
+    public List<LocationDto> Location(@PathVariable String token){
+        return locationService.getRecommendLocation(token);
+    }
+}
--- Backend/bfSearcher/src/main/java/softwareEngineering/bfSearcher/Service/LocationService.java
+++ Backend/bfSearcher/src/main/java/softwareEngineering/bfSearcher/Service/LocationService.java
@@ -8,12 +8,29 @@ import softwareEngineering.bfSearcher.Repository.LocationRepository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class LocationService {
     public final LocationRepository locationRepository;
+    public final UserService userService;
 
+    public List<LocationDto> getRecommendLocation (String token) {
+        var user = userService.Access(token);
+        var region = user.getLocationCategory().getLocation();
+        var disabledName = user.getDisabledCategory().getDisabled();
+        var hobby = user.getHobby();
+
+        List<LocationDto> recommendLocations = filterByDisabled(disabledName)
+                .stream()
+                .filter(location -> location.getAddress().contains(region))
+                .filter(location -> location.getHobbyCategory().getHobby().contains(hobby))
+                .map(this::entityToDto)
+                .collect(Collectors.toList());
+
+        return recommendLocations;
+    }
 
     public LocationDto entityToDto(Location location) {
         return LocationDto.builder()
@@ -44,8 +61,17 @@ public class LocationService {
 
     public List<LocationDto> getAllLocations(){
         List<LocationDto> locationDtos = new ArrayList<>();
-        locationRepository.findAll().forEach(Location ->{locationDtos.add(entityToDto(Location));});
+        locationRepository.findAll().forEach(Location -> locationDtos.add(entityToDto(Location)));
 
         return locationDtos;
     }
-}
+
+    private List<Location> filterByDisabled(String disabledName) {
+        return switch (disabledName) {
+            case "지체장애" -> locationRepository.findByParkForDisabledOrToiletForDisabledOrWheelchairRental(1, 1, 1);
+            case "뇌병변장애" -> locationRepository.findByDoorForDisabledOrToiletForDisabled(1, 1);
+            case "시각장애" -> locationRepository.findByBrailleGuideOrAudioGuideOrGuideDogPermitted(1, 1, 1);
+            default -> locationRepository.findAll();
+        };
+    }
+ }
