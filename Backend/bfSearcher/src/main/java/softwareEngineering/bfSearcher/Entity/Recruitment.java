d3da0b8be3134c45a83a86a2bc9929a9c5d3171b
ByeongChang <als6068@naver.com>
2023-11-13 17:23:44 +0900

    Recruitment 생성 후 저장을 위한 코드 작성
    Recruitment 생성 후 저장을 위한 코드 작성

--- Backend/bfSearcher/src/main/java/softwareEngineering/bfSearcher/Entity/Recruitment.java
+++ Backend/bfSearcher/src/main/java/softwareEngineering/bfSearcher/Entity/Recruitment.java
@@ -2,6 +2,7 @@ package softwareEngineering.bfSearcher.Entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +12,7 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Builder
 @Table(name = "recruitment")
 public class Recruitment {
 
---
+++ Backend/bfSearcher/src/main/java/softwareEngineering/bfSearcher/Repository/RecruitmentRepository.java
@@ -0,0 +1,15 @@
+package softwareEngineering.bfSearcher.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import softwareEngineering.bfSearcher.Entity.Recruitment;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface RecruitmentRepository extends JpaRepository<Recruitment, Long> {
+
+    @Override
+    Optional<Recruitment> findById(Long aLong);
+    Recruitment save(Recruitment recruitment);
+    List<Recruitment> findTop5ByOrderByIdDesc();
+}
